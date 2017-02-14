ecff390acb39e60753c20b230d9231c79018eed3
henson <henson.lu@gmail.com>
2017-02-14 13:42:35 +0800

    2017-02-14 13:42:34

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import "os/exec"
+import "time"
 
 func main() {
 	fetchAction()
 	checkOutAction()
 	mergeAction()
+	gitAdd()
+	gitCommit()
+	gitPush()
 }
 
 func fetchAction() {
@@ -43,6 +47,53 @@ func mergeAction() {
 	arg0 := "merge"
 	arg1 := "--allow-unrelated-histories"
 	arg2 := "upstream/gh-pages"
+	cmd := exec.Command(app, arg0, arg1, arg2)
+	out, err := cmd.Output()
+
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	print(string(out))
+}
+
+func gitAdd() {
+	app := "git"
+	arg0 := "add"
+	arg1 := "."
+	cmd := exec.Command(app, arg0, arg1)
+	out, err := cmd.Output()
+
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	print(string(out))
+}
+
+func gitCommit() {
+	app := "git"
+	arg0 := "commit"
+	arg1 := "-am"
+	arg2 := time.Now().Format("2006-01-02 15:04:05")
+	cmd := exec.Command(app, arg0, arg1, arg2)
+	out, err := cmd.Output()
+
+	if err != nil {
+		println(err.Error())
+		return
+	}
+
+	print(string(out))
+}
+
+func gitPush() {
+	app := "git"
+	arg0 := "push"
+	arg1 := "origin"
+	arg2 := "gh-pages"
 	cmd := exec.Command(app, arg0, arg1, arg2)
 	out, err := cmd.Output()
 
